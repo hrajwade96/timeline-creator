5ae541c02879fd429f8ec0d563cb57b2635ead5f
hrishikesh <hrishikesh.rajwade@tatadigital.com>
2024-08-31 20:04:38 +0530

    cli args support added

--- deps_analyzer/bin/deps_list.dart
+++ deps_analyzer/bin/deps_list.dart
@@ -6,6 +6,7 @@ import 'package:glob/list_local_fs.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 import 'package:yaml/yaml.dart';
+import 'package:args/args.dart';
 
 const projectPackage = 'project package';
 
@@ -55,11 +56,11 @@ bool shouldIgnore(String filePath, List<String> pathsToIgnore) =>
 ///
 /// Scans the project for `pubspec.yaml` files, fetches the latest versions
 /// of dependencies from pub.dev, and generates CSV, JSON, and text reports.
-Future<void> findDependencies() async {
+Future<void> findDependencies(
+    List<String> packages, List<String> formats, List<String> paths) async {
   final currentDir = Directory.current.path;
   final glob = Glob(path.join(currentDir, '**/pubspec.yaml'));
 
-  final pathsToIgnore = <String>[];
   var versionInfo = '';
   final allDependencies = <String, Set<String>>{};
 
@@ -67,7 +68,7 @@ Future<void> findDependencies() async {
     final relativeFilePath = path.relative(entity.path, from: path.current);
     final normalizedFilePath = path.normalize(relativeFilePath);
 
-    if (entity is File && !shouldIgnore(normalizedFilePath, pathsToIgnore)) {
+    if (entity is File && !shouldIgnore(normalizedFilePath, paths)) {
       try {
         final pubspecContent = await entity.readAsString();
         final doc = loadYaml(pubspecContent);
@@ -91,7 +92,10 @@ Future<void> findDependencies() async {
                 }
               }
 
-              if (versionInfo.contains('sdk: flutter')) {
+              List<String> checkList = ['sdk: flutter', ...packages];
+
+              // Check if versionInfo contains any string from the checkList
+              if (checkList.any((str) => versionInfo.contains(str))) {
                 safePrint('Skipping flutter SDK dependency');
                 return; // Skip adding flutter SDK version
               }
@@ -116,9 +120,20 @@ Future<void> findDependencies() async {
     latestVersions[packageName] = latestVersion;
   }
 
-  await generateCsvFile(allDependencies, latestVersions);
-  await generateJsonFile(allDependencies, latestVersions);
-  await generateTextFile(allDependencies, latestVersions);
+  for (String element in formats) {
+    switch (element) {
+      case 'json':
+        await generateJsonFile(allDependencies, latestVersions);
+        break;
+      case 'csv':
+        await generateCsvFile(allDependencies, latestVersions);
+        break;
+      case 'txt':
+        await generateTextFile(allDependencies, latestVersions);
+        break;
+      default:
+    }
+  }
 }
 
 /// Generates a CSV file listing all dependencies and their versions.
@@ -197,8 +212,31 @@ Future<void> generateTextFile(Map<String, Set<String>> allDependencies,
   safePrint('Text file generated: ${txtFile.path}');
 }
 
-Future<void> main() async {
+Future<void> main(List<String> arguments) async {
+  final parser = ArgParser()
+    ..addMultiOption('format',
+        abbr: 'f',
+        allowed: ['json', 'csv', 'text'],
+        help: 'Specify the output formats (json, csv, text).')
+    ..addMultiOption('skip-packages',
+        abbr: 's', help: 'Specify package names to skip during processing.')
+    ..addMultiOption('ignore-paths',
+        abbr: 'i', help: 'Specify path names to skip during processing.')
+    ..addFlag('help',
+        abbr: 'h', negatable: false, help: 'Display usage information.');
+
+  final argResults = parser.parse(arguments);
+  if (argResults['help'] as bool) {
+    safePrint(parser.usage);
+    exit(0);
+  }
+
+  final formats = argResults['format'] as List<String>? ?? ['text'];
+  final skipPackages = argResults['skip-packages'] as List<String>? ?? [];
+  final ignorePaths = argResults['ignore-paths'] as List<String>? ?? [];
+
   safePrint('Starting dependency analysis...');
-  await findDependencies();
+
+  await findDependencies(skipPackages, formats, ignorePaths);
   safePrint('Dependency analysis completed.');
 }
